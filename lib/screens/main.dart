b35b11b40064a83b1963ea4b0b019e574b679606
Filip ddd <rj23@protonmail.com>
2019-06-01 10:34:57 +0200

    feat: Basic folder structure

---
+++ lib/screens/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('DUPA'), // @TODO UNDUPA
+      ),
+    );
+  }
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,2 @@
+export 'default.dart';
+export 'amoled.dart';
